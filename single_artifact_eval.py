e11fc3ae61802cb42504d43dda061149d0964d1c
Isaac Robinson <isaac@roboflow.com>
2025-09-23 21:39:42 +0000

    basic sab for yolo

---
+++ single_artifact_eval.py
@@ -0,0 +1,53 @@
+import torch
+assert torch.cuda.is_available()
+
+import os
+os.environ["YOLO_SKIP_UPDATE"] = "1"
+
+from ultralytics import YOLO
+import roboflow
+
+from sab.models.utils import ArtifactBenchmarkRequest, run_benchmark_on_artifact
+from sab.models.benchmark_yolov8 import YOLOv8ONNXInference, YOLOv8TRTInference
+from sab.models.benchmark_yolov11 import YOLOv11ONNXInference
+
+
+def run_benchmark(dataset_name: str, dataset_base_dir: str, models_base_dir: str, model_name: str='yolov8n', max_dets: int=500, buffer_time: int=0.2):
+    # dirs are named differently in the models base dir so we need to search to get the matching one
+    for model_dir in os.listdir(models_base_dir):
+        if model_dir.startswith(dataset_name) and model_name in model_dir:
+            model_dir = os.path.join(models_base_dir, model_dir)
+            break
+
+    pt_location = os.path.join(model_dir, "supervised_reference", "weights", "best.pt")
+
+    model = YOLO(pt_location)
+
+    model.export(format="onnx", nms=True, conf=0.01)
+
+    onnx_location = os.path.join(model_dir, "supervised_reference", "weights", "best.onnx")
+
+    print(f"ONNX model exported to {onnx_location}")
+
+    benchmark_request = ArtifactBenchmarkRequest(
+        onnx_path=onnx_location,
+        inference_class=YOLOv8TRTInference,
+        needs_class_remapping=True,
+        needs_fp16=True,
+        max_dets=max_dets,
+        buffer_time=buffer_time
+    )
+
+    images_dir = os.path.join(dataset_base_dir, dataset_name, "test")
+    annotations_path = os.path.join(images_dir, "_annotations.coco.json")
+
+    results = run_benchmark_on_artifact(benchmark_request, images_dir, annotations_path)
+
+    print(results)
+    
+    
+
+
+if __name__ == "__main__":
+    import fire
+    fire.Fire(run_benchmark)
