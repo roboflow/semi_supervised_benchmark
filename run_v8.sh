9c629a1b465182c370120106306d90e2f08bf091
Matvezy <mpopov@trinity.edu>
2025-12-17 07:41:26 +0000

    log errors

--- run_v8.sh
+++ run_v8.sh
@@ -1,39 +1,50 @@
-python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=1 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=2 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=128 &
+#!/bin/bash
+FAIL_LOG="8_errors.txtx"
+> "$FAIL_LOG"
+
+run_and_log() {
+    "$@"
+    if [ $? -ne 0 ]; then
+        echo "FAILED: $*" >> "$FAIL_LOG"
+    fi
+}
+
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=1 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=2 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=128 &
 wait
 
-python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=1 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=2 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=64 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=1 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=2 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=64 &
 wait
 
-python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=1 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=16 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=32 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=1 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=16 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=32 &
 wait
 
-python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=2 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=4 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=128 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=2 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=4 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=128 &
 wait
 
-python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=4 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=4 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=128 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=4 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=4 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=128 &
 wait
 
-python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=32 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=32 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=8 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=32 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=32 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=8 &
 wait
 
-python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=8 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=16 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=64 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8m --skip_stac=True --batch=8 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=16 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=64 &
 wait
 
-python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=16 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=64 &
-python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=8 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=16 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8s --skip_stac=True --batch=64 &
+run_and_log python dispatcher.py stac.py url_list.txt --model_name=yolov8n --skip_stac=True --batch=8 &
 wait
